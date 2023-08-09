631ce337ae858cf71bc7524de109b3c842d27a89
chaeriIM <tldzmvpzl@naver.com>
2023-08-09 22:55:41 +0900

    구조 생성

---
+++ src/pages/FaceShapePage/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FaceShapePage = () => {
+  return (
+    <div>FaceShapePage</div>
+  )
+}
+
+export default FaceShapePage
---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../../components/Nav';
+
+const MainPage = () => {
+  return (
+    <div className='main'>
+      <Nav />
+    </div>
+  )
+}
+
+export default MainPage
