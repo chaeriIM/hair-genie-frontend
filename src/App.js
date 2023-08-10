d58545b4e71f30b58acba7df0434604585b0ee39
chaeriIM <tldzmvpzl@naver.com>
2023-08-10 16:59:13 +0900

    페이지 추가, Nav 구현

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainPage from './pages/MainPage';
 import FaceShapePage from './pages/FaceShapePage';
+import HairSynthesisPage from './pages/HairSynthesisPage';
+import SalonLocatorPage from './pages/SalonLocatorPage';
+import SalonReservationPage from './pages/SalonReservationPage';
+import MyPage from './pages/MyPage';
+import LoginPage from './pages/LoginPage'
+
 
 function App() {
   return (
     <div className='app'>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="hairsynthesis" element={<HairSynthesisPage />} />
         <Route path="faceshape" element={<FaceShapePage />} />
+        <Route path="salonlocator" element={<SalonLocatorPage />} />
+        <Route path="salonreservation" element={<SalonReservationPage />} />
+        <Route path="mypage" element={<MyPage />} />
+        <Route path="login" element={<LoginPage />} />
       </Routes>
     </div>
   );
