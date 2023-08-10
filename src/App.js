e06588c1a7ea4f074fc12a4a1f5b4ecffab8e4ae
VVjD <hj828@kakao.com>
2023-08-10 22:12:36 +0900

    병합 전 로컬 변경사항 커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+/* import MainPage from './pages/MainPage';
+import FaceShapePage from './pages/FaceShapePage'; */
+import HairSynthesisPage from './pages/HairSynthesisPage';
+/* import SalonLocatorPage from './pages/SalonLocatorPage'; */
+import SalonReservationPage from './pages/SalonReservationPage';
+/* import MyPage from './pages/MyPage';
+import LoginPage from './pages/LoginPage' */
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Routes>
+{/*         <Route path="/" element={<MainPage />} />
+ */}        <Route path="hairsynthesis" element={<HairSynthesisPage />} />
+{/*         <Route path="faceshape" element={<FaceShapePage />} />
+        <Route path="salonlocator" element={<SalonLocatorPage />} /> */}
+        <Route path="salonreservation" element={<SalonReservationPage />} />
+{/*         <Route path="mypage" element={<MyPage />} />
+        <Route path="login" element={<LoginPage />} /> */}
+      </Routes>
     </div>
   );
 }
