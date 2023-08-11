79934d96e97a568060e4427e541550f23a92d455
VVjD <hj828@kakao.com>
2023-08-11 16:37:34 +0900

    feature-B

--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Nav from '../../components/Nav'
+import './MyPage.css'
 
 const MyPage = () => {
   return (
-    <div>MyPage</div>
+    <div className='mypage'>
+      <Nav />
+      <p>마이페이지</p>
+      <hr className='separator' /> {/* 구분선을 추가하고 CSS 클래스를 지정 */}
+    </div>
   )
 }
 
