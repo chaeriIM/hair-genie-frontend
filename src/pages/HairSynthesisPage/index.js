30648b2fc93841c1abd68166348c561c6c960286
VVjD <hj828@kakao.com>
2023-08-11 16:40:01 +0900

    feature-B

--- src/pages/HairSynthesisPage/index.js
+++ src/pages/HairSynthesisPage/index.js
@@ -7,7 +7,7 @@ const HairSynthesisPage = () => {
     <div className='hairsynthesis'>
       <Nav />
       <p>Genie Time</p>
-
+      <hr className='separator' /> {/* 구분선을 추가하고 CSS 클래스를 지정 */}
     </div>
   )
 }
