85a6311a946d595e6808a4c6f63f51f3fb979a2b
VVjD <hj828@kakao.com>
2023-08-11 16:15:32 +0900

    feature-B

--- src/pages/SalonReservationPage/index.js
+++ src/pages/SalonReservationPage/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Nav from '../../components/Nav'
+import './SalonReservationPage.css'
 
 const SalonReservationPage = () => {
   return (
-    <div>SalonReservationPage</div>
-    
+    <div className='salonreservation'>
+      <Nav />
+      <p>미용실 예약</p>
+      <hr className='separator' /> {/* 구분선을 추가하고 CSS 클래스를 지정 */}
+    </div>
   )
 }
 
