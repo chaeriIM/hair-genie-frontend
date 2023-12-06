f75466d1763820d564fb49cf614b190fbbab10d6
VVjD <hj828@kakao.com>
2023-12-06 21:50:43 +0900

    카테고리 추가

--- src/pages/NoticeBoardPage/NoticeBoard.js
+++ src/pages/NoticeBoardPage/NoticeBoard.js
@@ -10,10 +10,21 @@ const NoticeBoard = () => {
     const [posts, setPosts] = useState([]);
     const [users, setUsers] = useState({});
 
+    const [filteredPosts, setFilteredPosts] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('전체');
+    const [boardTitle, setBoardTitle] = useState('전체 글 보기');
+    const [isCategoryListOpen, setIsCategoryListOpen] = useState(false);
+
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/board/')
             .then(response => {
-                setPosts(response.data);
+                const sortedPosts = response.data.sort((a, b) => {
+                    const dateA = new Date(a.created_at);
+                    const dateB = new Date(b.created_at);
+                    return dateB - dateA;
+                });
+                setPosts(sortedPosts);
+                setFilteredPosts(sortedPosts);
 
                 // user 정보 가져오기
                 const userIds = response.data.map(post => post.customer);
@@ -46,6 +57,34 @@ const NoticeBoard = () => {
         return `${year}.${month}.${day}`;
     };
 
+    const handlePostClick = (postId) => {
+        // 게시글을 클릭할 때마다 조회수를 업데이트하는 요청
+        axios.put(`http://127.0.0.1:8000/board/${postId}/increment-views/`)
+            .then(updateResponse => {
+                // 조회수가 업데이트된 게시물 정보를 받아옴
+                // 이 정보는 BoardDetail에서 사용될 수 있음
+                console.log('Updated views count:', updateResponse.data);
+            })
+            .catch(error => {
+                console.error('Error updating views count:', error);
+            });
+    };
+
+    const toggleCategoryList = () => {
+        setIsCategoryListOpen(!isCategoryListOpen);
+    };
+
+    const filterPostsByCategory = (category, title) => {
+        if (category === '전체') {
+            setFilteredPosts(posts);
+        } else {
+            const filtered = posts.filter(post => post.category === category);
+            setFilteredPosts(filtered);
+        }
+        setSelectedCategory(category);
+        setBoardTitle(title);
+    };
+
     return (
         <div className='noticeboard'>
             <Nav />
@@ -54,37 +93,62 @@ const NoticeBoard = () => {
             <hr />
             <div className='body-container'>
                 <div className='board-top-container'>
-                    <p className='board-title'>전체 글 보기</p>
+                    <div className='board-title-container'>
+                        <span className='board-title' onClick={toggleCategoryList}>{boardTitle}</span>
+                        <span className={`category-arrow${isCategoryListOpen ? ' open' : ''}`} onClick={toggleCategoryList}></span>
+                        <div className={`category-filter${isCategoryListOpen ? ' open' : ''}`}>
+                            <span className='category' onClick={() => filterPostsByCategory('전체', '전체 글 보기')}>전체</span>
+                            <span className='category' onClick={() => filterPostsByCategory('공지', '공지')}>공지</span>
+                            <span className='category' onClick={() => filterPostsByCategory('자유', '자유')}>자유</span>
+                            <span className='category' onClick={() => filterPostsByCategory('미용실 등록 요청', '미용실 등록 요청')}>미용실 등록 요청</span>
+                        </div>
+                    </div>
                     <Link to="/boardwrite">
-                        <button className='login-btn' style={{ width: '80px' }}>글쓰기</button>
+                        <button className='login-btn' style={{ width: '70px' }}>글쓰기</button>
                     </Link>
                 </div>
                 <hr className="board-separator" />
-                <div className='board-list-container'>
-                    {/* 전체 글 목록 테이블 */}
-                    <table className='board-table'>
-                        <thead>
-                            <tr className='board-item-container'>
-                                <th className='board-item'></th>
-                                <th className='board-item'>제목</th>
-                                <th className='board-item'>작성자</th>
-                                <th className='board-item'>작성일</th>
-                                <th className='board-item'>조회</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {posts.map((post, index) => (
-                                <tr className='board-detail-container' key={post.id}>
-                                    <td className='board-detail'>{index + 1}</td>
-                                    <td className='board-detail' style={{ textAlign: 'left' }}>
-                                        <Link to={`/noticeboard/${post.id}`}>{post.title}</Link></td>
-                                    <td className='board-detail'>{users[post.customer]?.unickname}</td>
-                                    <td className='board-detail'>{formatDate(post.created_at)}</td>
-                                    <td className='board-detail'>{post.views_count}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                <div className='board-body-container'>
+                    <div className='board-list-container'>
+                        {filteredPosts.length === 0 ? (
+                            <p className='no-posts-message'>등록된 게시글이 없습니다.</p>
+                        ) : (
+                            <table className='board-table'>
+                                <thead>
+                                    <tr className='board-item-container'>
+                                        <th className='board-item'></th>
+                                        <th className='board-item'>제목</th>
+                                        <th className='board-item'>작성자</th>
+                                        <th className='board-item'>작성일</th>
+                                        <th className='board-item'>조회</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {filteredPosts.map((post, index) => (
+                                        <tr className='board-detail-container' key={post.id}>
+                                            {selectedCategory === '전체' ? (
+                                                <td className='board-detail'>{post.category}</td>
+                                            ) : (
+                                                <td className='board-detail'>{post.id}</td>
+                                            )}
+                                            <td className='board-detail' style={{ textAlign: 'left' }}>
+                                                <Link
+                                                    className='board-list-title'
+                                                    to={`/noticeboard/${post.id}`}
+                                                    onClick={() => handlePostClick(post.id)}
+                                                >
+                                                    {post.title}
+                                                </Link>
+                                            </td>
+                                            <td className='board-detail'>{users[post.customer]?.unickname}</td>
+                                            <td className='board-detail'>{formatDate(post.created_at)}</td>
+                                            <td className='board-detail'>{post.views_count}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
